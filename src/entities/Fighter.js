182fddad4512f2b35ae87e54e136d9e73109183b
axell152 <axell.nicol@outlook.fr>
2026-09-04 09:59:12 +0200

    Update Fighter.js

--- src/entities/Fighter.js
+++ src/entities/Fighter.js
@@ -85,12 +85,11 @@ export class Fighter {
 
    scene.physics.add.existing(this.container);
     
-    // La hitbox fait toute la hauteur du sprite (h) et sa largeur (w)
-    const bodyWidth = w * 0.6; // Ajustez la largeur de la hitbox si besoin
+    const bodyWidth = w * 0.6; 
     const bodyHeight = h;
     
+    // Centre la hitbox horizontalement et place sa base exactement sur le point 0 du conteneur (les pieds)
     this.container.body.setSize(bodyWidth, bodyHeight);
-    // -bodyHeight place le bas de la boîte exactement sur le point 0 du conteneur (les pieds au sol)
     this.container.body.setOffset(-bodyWidth / 2, -bodyHeight);
     
     this.container.body.setCollideWorldBounds(true);
